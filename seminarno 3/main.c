acf5bb0db9d4e39235d0a1c9ebc2a6827d861adb
Antonio Burgov <119887445+AntonioBurgov@users.noreply.github.com>
2024-04-07 17:41:56 +0300

    Fourth Project

---
+++ seminarno 3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    int num;
+    int max_num = -2147483648;
+    int min_num = 2147483647;
+    printf("Enter numbers (Write 0 to end input):\n");
+
+
+    do {
+        scanf("%d", &num);
+
+        if (num != 0) {
+            max_num = max(max_num, num);
+            min_num = min(min_num, num);
+        }
+    } while (num != 0);
+
+    printf("Maximum number is: %d\n", max_num);
+    printf("Minimum number is: %d\n", min_num);
+
+    return 0;
+}
